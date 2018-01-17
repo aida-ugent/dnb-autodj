2b4ab83fce9d6ae627a4a36b18c17f621e08f207
Len <len.vandeveire@gmail.com>
2018-01-17 10:46:15 +0100

    Update copyright info bis

--- Application/song.py
+++ Application/song.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Len Vande Veire, IDLab, Department of Electronics and Information Systems, Ghent University
-# This file is part of the source code for the Auto-DJ research project, published in Vande Veire, Len, and De Bie, Tijl, "From raw audio to a seamless mix: an artificial intelligence approach to creating an automated DJ system." EURASIP,  2018 (submitted)
+# This file is part of the source code for the Auto-DJ research project, published in Vande Veire, Len, and De Bie, Tijl, "From raw audio to a seamless mix: an artificial intelligence approach to creating an automated DJ system.", 2018 (submitted)
 # Released under AGPLv3 license.
 
 from util import *
